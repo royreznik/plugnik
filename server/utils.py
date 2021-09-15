623737b7bf58530dd5cb25d29ee85865ed652cb8
royreznik <royreznik@gmail.com>
2021-09-15 08:11:17 -0700

    Adding route for uploading a zip plugin

--- server/utils.py
+++ server/utils.py
@@ -7,11 +7,18 @@ from lxml.etree import _ElementTree as ElementTree, XMLParser
 xml_parser = etree.XMLParser(remove_blank_text=True)
 
 
-def add_new_plugin(
-    plugins_tree: ElementTree, jar_fileobject: IO, jar_location: Path
+def add_new_zip_plugin(plugins_tree: ElementTree, zip_fileobject: IO, zip_name: Path, plugin_file_name: str) -> None:
+    zip_file = zipfile.ZipFile(zip_fileobject)
+    jar_plugin_name = zip_name.with_suffix(".jar").name
+    jar_plugin_path = [i for i in zip_file.namelist() if jar_plugin_name in i][0]
+    add_new_jar_plugin(plugins_tree, zip_file.open(jar_plugin_path), plugin_file_name)
+
+
+def add_new_jar_plugin(
+    plugins_tree: ElementTree, jar_fileobject: IO, plugin_file_name: str
 ) -> None:
     plugin_metadata = _get_plugin_metadata_from_jar(jar_fileobject)
-    new_plugin_xml = _generate_plugin_xml_from_metadata(plugin_metadata, jar_location)
+    new_plugin_xml = _generate_plugin_xml_from_metadata(plugin_metadata, plugin_file_name)
     _dump_new_plugin(plugins_tree, new_plugin_xml)
 
 
@@ -26,14 +33,14 @@ def _get_plugin_metadata_from_jar(jar_fileobject: IO) -> ElementTree:
 
 
 def _generate_plugin_xml_from_metadata(
-    plugin_metadata: ElementTree, jar_location: Path
+    plugin_metadata: ElementTree, plugin_file_name: str
 ) -> ElementTree:
     # noinspection PyUnresolvedReferences
     new_plugin_xml = etree.Element(
         "plugin",
         attrib={
             "id": plugin_metadata.find("id").text,
-            "url": str(jar_location),
+            "url": "/get_plugin/" + plugin_file_name, # TODO: Make this better
             "version": plugin_metadata.find("version").text,
         },
     )
