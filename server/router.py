175e29f0bf90360f9c8c0009f0ffd2f0a053dcb8
royreznik <reznik@gmail.com>
2021-09-16 13:35:21 -0700

    Refactor router and extract to utils

--- server/router.py
+++ server/router.py
@@ -1,10 +1,9 @@
 import io
-from http import HTTPStatus
 from pathlib import Path
 from typing import Any
 from zipfile import BadZipfile
 
-from fastapi import FastAPI, File, HTTPException, Response, UploadFile
+from fastapi import FastAPI, File, Response, UploadFile
 from fastapi.responses import FileResponse
 from lxml import etree
 
@@ -13,9 +12,18 @@ from lxml.etree import _ElementTree as ElementTree
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
-from .plugin_manager import add_new_jar_plugin, add_new_zip_plugin, xml_parser, remove_plugin_xml, remove_plugin_file
+from .plugin_manager import (
+    add_new_jar_plugin,
+    add_new_zip_plugin,
+    remove_plugin_file,
+    remove_plugin_xml,
+    save_plugin_file,
+    xml_parser,
+)
 from .settings import settings
+from .utils import validate_extension
 
+# TODO: change numbers to httpstatus
 app = FastAPI()
 plugins_tree: ElementTree = etree.parse(str(settings.plugins_xml), xml_parser)
 
@@ -39,28 +47,23 @@ async def get_plugin(name: str):
 @app.post("/upload_jar", status_code=201)
 async def upload(plugin: UploadFile = File(...)):
     filename = Path(plugin.filename)
-    if not filename.suffix == ".jar":
-        raise HTTPException(status_code=400, detail="You must upload a .jar file!")
+    validate_extension(filename, ".jar")
     jar_data = await plugin.read()
-    if isinstance(jar_data, str):
-        jar_data = jar_data.encode()
+    jar_data = jar_data if isinstance(jar_data, bytes) else jar_data.encode()
     jar_fileobject = io.BytesIO(jar_data)
     plugin_file_name = add_new_jar_plugin(plugins_tree, jar_fileobject, filename.name)
-    (settings.plugins_folder / plugin_file_name).write_bytes(jar_data)
+    save_plugin_file(plugin_file_name, jar_data)
 
 
 @app.post("/upload_zip", status_code=201)
-async def upload_zip(plugin: UploadFile = File(...)):
-    filename = Path(plugin.filename)
-    if not filename.suffix == ".zip":
-        raise HTTPException(status_code=400, detail="You must upload a .zip file!")
-
-    zip_data = await plugin.read()
-    if isinstance(zip_data, str):
-        zip_data = zip_data.encode()
+async def upload_zip(plugin_file: UploadFile = File(...)):
+    file_name = Path(plugin_file.filename)
+    validate_extension(file_name, ".zip")
+    zip_data = await plugin_file.read()
+    zip_data = zip_data if isinstance(zip_data, bytes) else zip_data.encode()
     zip_fileobject = io.BytesIO(zip_data)
-    plugin_file_name = add_new_zip_plugin(plugins_tree, zip_fileobject, filename)
-    (settings.plugins_folder / plugin_file_name).write_bytes(zip_data)
+    plugin_file_name = add_new_zip_plugin(plugins_tree, zip_fileobject, file_name)
+    save_plugin_file(plugin_file_name, zip_data)
 
 
 @app.delete("/", status_code=204, response_class=Response)
