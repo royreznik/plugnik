0c2506c74b911c9aca5e5b2dfa3104ea79d7eeb4
royreznik <royreznik@gmail.com>
2021-09-16 14:13:33 -0700

    now using HTTPStatus for codes

--- server/router.py
+++ server/router.py
@@ -1,4 +1,5 @@
 import io
+from http import HTTPStatus
 from pathlib import Path
 from typing import Any
 from zipfile import BadZipfile
@@ -37,9 +38,8 @@ async def get_plugin(name: str):
     return FileResponse(get_plugin_file_path(name))
 
 
-# TODO: add ability to delete plugin
 # TODO: don't allow multiply copy of the same plugin
-@app.post("/upload_jar", status_code=201)
+@app.post("/upload_jar", status_code=HTTPStatus.CREATED)
 async def upload_jar(plugin_file: UploadFile = File(...)):
     filename = Path(plugin_file.filename)
     validate_extension(filename, ".jar")
@@ -50,7 +50,7 @@ async def upload_jar(plugin_file: UploadFile = File(...)):
     save_plugin_file(plugin_file_name, jar_data)
 
 
-@app.post("/upload_zip", status_code=201)
+@app.post("/upload_zip", status_code=HTTPStatus.CREATED)
 async def upload_zip(plugin_file: UploadFile = File(...)):
     file_name = Path(plugin_file.filename)
     validate_extension(file_name, ".zip")
@@ -61,7 +61,7 @@ async def upload_zip(plugin_file: UploadFile = File(...)):
     save_plugin_file(plugin_file_name, zip_data)
 
 
-@app.delete("/", status_code=204, response_class=Response)
+@app.delete("/", status_code=HTTPStatus.NO_CONTENT, response_class=Response)
 async def delete_plugin(plugin: str, version: str):
     plugin = remove_plugin_xml(plugin, version)
     remove_plugin_file(plugin)
@@ -72,6 +72,6 @@ async def delete_plugin(plugin: str, version: str):
 @app.exception_handler(ValueError)
 async def value_exception_handler(request: Request, exc: Any):
     return JSONResponse(
-        status_code=400,
+        status_code=HTTPStatus.BAD_REQUEST,
         content={"exception": str(exc)},
     )
