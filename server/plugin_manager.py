2fd25d1a7eb18c987bbcdc4fdec6a120cb95eac0
royreznik <reznik@gmail.com>
2021-09-16 14:04:54 -0700

    Changing to plugin manager settings

--- server/plugin_manager.py
+++ server/plugin_manager.py
@@ -8,14 +8,14 @@ from lxml import etree
 from lxml.etree import _Element as Element
 from lxml.etree import _ElementTree as ElementTree
 
-# TODO: Maybe remove the settings from here and just pass the settings?
-from .settings import settings
+from .settings import plugin_manager_settings
 
 xml_parser = etree.XMLParser(remove_blank_text=True)
+plugins_tree: ElementTree = etree.parse(str(plugin_manager_settings.plugins_xml), xml_parser)
 
 
 def remove_plugin_xml(
-    plugins_tree: ElementTree, plugin_name: str, version: str
+    plugin_name: str, version: str
 ) -> Element:
     plugin_search_query = (
         f'.//plugin[@version="{version}"]//name[text()="{plugin_name}"]'
@@ -26,33 +26,41 @@ def remove_plugin_xml(
         raise ValueError(f"Plugin not found. name: {plugin_name}, version: {version}")
     plugin = plugin_search_result[0].getparent()
     plugins_tree.getroot().remove(plugin)
-    plugins_tree.write(str(settings.plugins_xml), pretty_print=True)
+    plugins_tree.write(str(plugin_manager_settings.plugins_xml), pretty_print=True)
     return plugin
 
 
-def remove_plugin_file(plugin_folder: Path, plugin: Element) -> None:
+def remove_plugin_file(plugin: Element) -> None:
     plugin_url = plugin.attrib["url"]
     plugin_file_name = Path(plugin_url).name
-    (plugin_folder / plugin_file_name).unlink(missing_ok=True)
+    (plugin_manager_settings.plugins_folder / plugin_file_name).unlink(missing_ok=True)
 
 
 def save_plugin_file(plugin_file_name: str, plugin_data: bytes):
-    (settings.plugins_folder / plugin_file_name).write_bytes(plugin_data)
+    (plugin_manager_settings.plugins_folder / plugin_file_name).write_bytes(plugin_data)
+
+
+def get_plugin_file_path(name: str) -> Path:
+    return plugin_manager_settings.plugins_folder / name
+
+
+def get_all_plugins_xml_string() -> str:
+    return etree.tostring(plugins_tree.getroot())
 
 
 def add_new_zip_plugin(
-    plugins_tree: ElementTree, zip_fileobject: IO, zip_name: Path
+    zip_fileobject: IO, zip_name: Path
 ) -> str:
     plugin_archive = zipfile.ZipFile(zip_fileobject)
     inner_jar_name = zip_name.with_suffix(".jar").name
     inner_jar_path = [i for i in plugin_archive.namelist() if inner_jar_name in i][0]
     return add_new_jar_plugin(
-        plugins_tree, plugin_archive.open(inner_jar_path), zip_name.name
+        plugin_archive.open(inner_jar_path), zip_name.name
     )
 
 
 def add_new_jar_plugin(
-    plugins_tree: ElementTree, jar_fileobject: IO, plugin_file_name: str
+    jar_fileobject: IO, plugin_file_name: str
 ) -> str:
     plugin_metadata = _get_plugin_metadata_from_jar(jar_fileobject)
     plugin_version = plugin_metadata.find("version").text
@@ -60,7 +68,7 @@ def add_new_jar_plugin(
     new_plugin_xml = _generate_plugin_xml_from_metadata(
         plugin_metadata, plugin_file_name, plugin_version
     )
-    _dump_new_plugin_xml(plugins_tree, new_plugin_xml)
+    _dump_new_plugin_xml(new_plugin_xml)
     return plugin_file_name
 
 
@@ -92,8 +100,8 @@ def _generate_plugin_xml_from_metadata(
 
 
 def _dump_new_plugin_xml(
-    plugins_tree: ElementTree, new_plugin_xml: ElementTree
+    new_plugin_xml: ElementTree
 ) -> None:
     # noinspection PyUnresolvedReferences
     plugins_tree.getroot().append(new_plugin_xml)
-    plugins_tree.write(str(settings.plugins_xml), pretty_print=True)
+    plugins_tree.write(str(plugin_manager_settings.plugins_xml), pretty_print=True)
--- server/router.py
+++ server/router.py
@@ -5,10 +5,8 @@ from zipfile import BadZipfile
 
 from fastapi import FastAPI, File, Response, UploadFile
 from fastapi.responses import FileResponse
-from lxml import etree
 
 # noinspection PyProtectedMember
-from lxml.etree import _ElementTree as ElementTree
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
@@ -17,15 +15,12 @@ from .plugin_manager import (
     add_new_zip_plugin,
     remove_plugin_file,
     remove_plugin_xml,
-    save_plugin_file,
-    xml_parser,
+    save_plugin_file, get_plugin_file_path, get_all_plugins_xml_string,
 )
-from .settings import settings
 from .utils import validate_extension
 
 # TODO: change numbers to httpstatus
 app = FastAPI()
-plugins_tree: ElementTree = etree.parse(str(settings.plugins_xml), xml_parser)
 
 
 # TODO: Can optimize the response size if we look at the build number and filter
@@ -33,25 +28,25 @@ plugins_tree: ElementTree = etree.parse(str(settings.plugins_xml), xml_parser)
 @app.get("/")
 async def all_plugins(build: str) -> Response:
     return Response(
-        content=etree.tostring(plugins_tree.getroot()), media_type="application/xml"
+        content=get_all_plugins_xml_string(), media_type="application/xml"
     )
 
 
 @app.get("/get_plugin/{name}")
 async def get_plugin(name: str):
-    return FileResponse(settings.plugins_folder / name)
+    return FileResponse(get_plugin_file_path(name))
 
 
 # TODO: add ability to delete plugin
 # TODO: don't allow multiply copy of the same plugin
 @app.post("/upload_jar", status_code=201)
-async def upload(plugin: UploadFile = File(...)):
-    filename = Path(plugin.filename)
+async def upload_jar(plugin_file: UploadFile = File(...)):
+    filename = Path(plugin_file.filename)
     validate_extension(filename, ".jar")
-    jar_data = await plugin.read()
+    jar_data = await plugin_file.read()
     jar_data = jar_data if isinstance(jar_data, bytes) else jar_data.encode()
     jar_fileobject = io.BytesIO(jar_data)
-    plugin_file_name = add_new_jar_plugin(plugins_tree, jar_fileobject, filename.name)
+    plugin_file_name = add_new_jar_plugin(jar_fileobject, filename.name)
     save_plugin_file(plugin_file_name, jar_data)
 
 
@@ -62,14 +57,14 @@ async def upload_zip(plugin_file: UploadFile = File(...)):
     zip_data = await plugin_file.read()
     zip_data = zip_data if isinstance(zip_data, bytes) else zip_data.encode()
     zip_fileobject = io.BytesIO(zip_data)
-    plugin_file_name = add_new_zip_plugin(plugins_tree, zip_fileobject, file_name)
+    plugin_file_name = add_new_zip_plugin(zip_fileobject, file_name)
     save_plugin_file(plugin_file_name, zip_data)
 
 
 @app.delete("/", status_code=204, response_class=Response)
 async def delete_plugin(plugin: str, version: str):
-    plugin = remove_plugin_xml(plugins_tree, plugin, version)
-    remove_plugin_file(settings.plugins_folder, plugin)
+    plugin = remove_plugin_xml(plugin, version)
+    remove_plugin_file(plugin)
 
 
 # noinspection PyUnusedLocal
