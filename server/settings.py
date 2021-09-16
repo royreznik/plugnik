6dc5a3e86284638348c1637a634589866f47ce9b
royreznik <royreznik@gmail.com>
2021-09-16 14:04:54 -0700

    Changing to plugin manager settings

--- server/settings.py
+++ server/settings.py
@@ -4,10 +4,10 @@ from pathlib import Path
 from pydantic import BaseSettings
 
 
-class Settings(BaseSettings):
+class PluginManagerSettings(BaseSettings):
     plugins_xml: Path = Path("/updatePlugins.xml")
     plugins_folder: Path = Path("/tmp/plugins/")
 
 
 with open("/app/settings.json") as f:
-    settings = Settings(**json.load(f))
+    plugin_manager_settings = PluginManagerSettings(**json.load(f))
--- server/utils.py
+++ server/utils.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 
 def validate_extension(file_name: Path, extension: str) -> None:
-    if not file_name.suffix != extension:
+    if file_name.suffix != extension:
         raise ValueError(f"File with {extension} extension required")
